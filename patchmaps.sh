ab74181f7ed9fd2001c98487b0c59b310b8dbb5e
Isaac <tux314159@gmail.com>
2024-03-20 20:36:01 +0800

    Implement ruleset stripping

--- patchmaps.sh
+++ patchmaps.sh
@@ -1,35 +1,38 @@
-#! /bin/bash
+#!/bin/sh
 
 # USAGE: ./modmaps.sh <balance pack> <file of map IDs>
 #
 # Adds rules in map mod pack to any maps you want. Also
 # optionally adds an overlay to maps.
 
-# Configuration.
+# Configuration
 max_download_workers=18  # maximum number of maps to download at once
 patched_dest="patched"  # where to place patched maps
+ramod_dir="/usr/lib/openra/mods/ra"  # location of RA mod rules
+
+rm -rf .work
 
 # ===============================
+# Patch the maps
+# ===============================
 
-shopt -s nullglob
-
-# Take stuff from args.
+# Take stuff from args
 { [ "$1" ] && [ "$2" ] ; } || { >&2 echo "USAGE: ./modmaps.sh <balance pack> <file of map IDs>"; exit 1; }
 bal_pack="$1"
 map_ids_file="$2"
 
-# unzip but STFU.
+# unzip but STFU
 unzip_() {
 	unzip "$1" >/dev/null
 }
 
-# Some output escape codes.
-t_clrln="\r\x1b[K"
+# Some output escape codes
+t_clrln="\r\033[K"
 t_bold=$(tput bold)
 t_ital=$(tput sitm)
 t_norm=$(tput sgr0)
 
-# Create temporary working structures.
+# Create temporary working structures
 mkdir -p .work
 mkdir -p .work/maps_fresh
 mkdir -p .work/maps_unpacked
@@ -37,17 +40,17 @@ mkdir -p .work/balpack
 rm -rf "$patched_dest"
 mkdir -p "$patched_dest"
 
-# Read map ids and download.
-# Only numbers starting exactly on a new line will be considered. The rest of the line
-# will be treated as a comment.
+# Read map ids and download
+# Only numbers starting exactly on a new line will be considered; the rest of the line
+# will be treated as a comment
 rm -rf .work/dlcmds*
 
 map_ids=$(cut -d' ' -f1 "$map_ids_file")
 touch .work/dlcmds  # don't break if there's no IDs
-# Command-lines for downloading the files.
+# Command-lines for downloading the files
 for id in $map_ids; do
 	# We grab the title first, then use that as the filename with which to save the map,
-	# replacing all spaces with underscores.
+	# replacing all spaces with underscores
 	cat <<-EOF >>.work/dlcmds
 	printf "#$id, "
 	curl -s\
@@ -57,63 +60,88 @@ for id in $map_ids; do
 	EOF
 done
 
-# Batch downloads.
+# Batch downloads
 max_download_workers=$((max_download_workers * 2))	# *2 because each dl is actually two lines
 split -d -l$max_download_workers .work/dlcmds .work/dlcmds_batch
 for script in .work/dlcmds_batch*; do
-	echo "printf '\x1b[2D)';wait" >>"$script"
+	printf "%s\n" "printf '\x1b[2D)';wait" >>"$script"
 	printf "${t_clrln}${t_bold}Downloading maps${t_norm}... ("
-	bash "$script"
+	sh "$script"
 done
 printf "${t_clrln}${t_bold}Downloading maps${t_norm}... ${t_ital}done.${t_norm}\n"
 
-# Unzip all maps.
+# Unzip all maps
 for map in .work/maps_fresh/*; do
 	mkdir ".work/maps_unpacked/$(basename "$map" .oramap)"
 	cp "$map" ".work/maps_unpacked/$(basename "$map" .oramap)"
 	(cd ".work/maps_unpacked/$(basename "$map" .oramap)"; unzip_ ./*; rm "$(basename "$map")")
 done
 
-# Unzip balance pack.
+# Unzip balance pack
 # Each top-level directory in the balance pack represents a key to add to
 # or modify in map.yaml, and the filenames of each file in there will be
-# added.
+# added
 cp "$bal_pack" .work/balpack/
 (cd .work/balpack; unzip_ ./*;)
 rm ".work/balpack/$(basename "$bal_pack")"
 
-# Look at all the top-level dirs, representing top-level YAML keys.
+# Look at all the top-level dirs, representing top-level YAML keys
 keys="$(find .work/balpack -mindepth 1 -maxdepth 1 -type 'd' -exec basename {} \;)"
+# Get all the default rules and weapons
+baserules=$(find "$ramod_dir/rules" "$ramod_dir/weapons" -name '*.yaml' -exec grep -E '^[^[:space:]]+:[ \t]?$' {} \;)
 
+# Process each map
 for mapdir in .work/maps_unpacked/*; do
 	printf "${t_clrln}${t_bold}Patching map YAML(s)${t_norm}... (%s)" "$(basename "$mapdir")"
 	mapyaml="$mapdir/map.yaml"
-	# Copy YAMLs over.
+
+	# Go through yamls and delete keys that exist in baserules
+	for key in $baserules; do
+		printf "%s\n" "/^$key[^ \t]\?$/,/^[^[:blank:]]/-1d" >>.work/edscript
+	done
+	printf "wq\n" >>.work/edscript
+	find "$mapdir" -name "*.yaml" -exec sh -c 'ed $1 <.work/edscript 2&>/dev/null' sh {} \;
+
+	# Copy YAMLs over
 	find ".work/balpack/" -type 'f' -exec cp {} "$mapdir" \;
-	# Include them in map.yaml.
+	# Include them in map.yaml
 	for key in $keys; do
-		# Remove stupid carriage returns.
+		# Remove stupid carriage returns
 		tr -d '\r' <"$mapyaml" >"$mapyaml"_
 		mv "$mapyaml"_ "$mapyaml"
 		yamls="$(find ".work/balpack/$key" -type 'f' -exec basename {} \; | tr '\n' ',')"
-		# If there is no such key, add it.
-		grep "^$key:" "$mapyaml" >/dev/null || echo -e "\n$key: " >>"$mapyaml"
-		# Append all YAML filenames to that key.
+		# If there is no such key, add it
+		grep "^$key:" "$mapyaml" >/dev/null || printf "\n%s: " "$key" >>"$mapyaml"
+		# Append all YAML filenames to that key and remove duplicates
 		ed "$mapyaml" <<-EOF >/dev/null
 			/^$key:/a
-			, $yamls
+			,$yamls
 			.
 			-1,.j
-			s/: , /: /
-			s/,$//
+			s/:,\?/:/
+			s/,\?[[:space:]]*$//
+			s/\(.\)\(,\)\? \?/\1\2/g
+			w
+			\$a
+			/^$key:/s/\([:,]\)\(.\+\.yaml\)\(.\+\)\2/\1\2\3/
+			w
+			!exec ed "%" <.work/rgxp
+			q
+			.
+			-3,\$w .work/rgxp
+			!exec ed "%" <.work/rgxp
+			E
+			/^$key:/s/:,\?/:/
+			s/,*$//
+			w
+			s/,\+/,/g
 			wq
 		EOF
-	true
 	done
 done
 printf "${t_clrln}${t_bold}Patching map YAMLs${t_norm}... ${t_ital}done.${t_norm}\n"
 
-# Composite the map preview overlay on.
+# Composite the map preview overlay on
 if [ -f ".work/balpack/overlay.png" ]; then
 	for mapdir in .work/maps_unpacked/*; do
 		printf "${t_clrln}${t_bold}Compositing map previews${t_norm}... (%s)" "$(basename "$mapdir")"
@@ -123,7 +151,7 @@ if [ -f ".work/balpack/overlay.png" ]; then
 	printf "${t_clrln}${t_bold}Compositing map previews${t_norm}... ${t_ital}done.${t_norm}\n"
 fi
 
-# Zip the patched maps.
+# Zip the patched maps
 for mapdir in .work/maps_unpacked/*; do
 	printf "${t_clrln}${t_bold}Zipping patched maps${t_norm}... (%s)" "$(basename "$mapdir")"
 	(cd "$mapdir"; zip -r "$(basename "$mapdir")".oramap ./* >/dev/null)
@@ -135,4 +163,4 @@ printf "${t_clrln}${t_bold}Zipping patched maps${t_norm}... ${t_ital}done.${t_no
 printf "\n${t_ital}All done. Patched ${t_bold}%d${t_norm}${t_ital} maps.${t_norm}\n" "$(find .work/maps_unpacked -mindepth 1 -maxdepth 1 | wc -l)"
 
 # Clean up.
-rm -rf .work
+#rm -rf .work
